ed74ae2fc48fa2061b28a3e8d6f4352e709b9a6f
sushektamrakar <sushek_3@hotmail.com>
2023-07-26 00:38:06 +0545

    feat(job): create recommendation feature

--- apps/job/api/v1/views.py
+++ apps/job/api/v1/views.py
@@ -2,12 +2,12 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from django.db.models import Q, Case, When
 
 from apps.core.viewsets import CreateListUpdateDestroyViewSet, CreateListUpdateViewSet
 from apps.job.api.v1.serializer import CategorySerializer, IndustrySerializer, JobAppledSerializer, JobSerializer, TrainingAppledSerializer, TrainingSerializer
 from apps.job.models import Category, Industry, Job, JobApplied, Training, TrainingApplied
 from apps.job.utils import compute_similarity
-
 
 
 class IndustryViewSet(viewsets.ModelViewSet):
@@ -41,14 +41,22 @@ class JobViewSet(CreateListUpdateDestroyViewSet):
     def recommendation_job(self, request, *args, **kwargs):
         qs = self.get_queryset()
         user_skills = [skill.name for skill in request.user.skills.all()]
-        query_result = qs
-        if user_skills:
-            query_result = query_result.annotate(distance=compute_similarity("description", user_skills))
-            query_result = query_result.filter(distance__lte=0.7).order_by("-distance")
-        else:
-            query_result = []
-       
-        serializer = serializer = self.get_serializer(query_result, many=True)
+        skill_str = ''
+        for skill in request.user.skills.all():
+            skill_str += str(skill.name)
+
+
+        qs = qs.filter(Q(skills_required__name__in=user_skills) or Q(description__icontains=skill_str)).annotate(
+            recommendation_priority=Case(
+                When(Q(description__icontains=skill_str), then=1),
+                When(Q(skills_required__name__in=user_skills), then=2),
+                When(Q(skills_required__name__in=user_skills) & Q(Q(description__icontains=skill_str)), then=3),
+                default=0
+                )
+
+        ).order_by("-recommendation_priority")
+
+        serializer = serializer = self.get_serializer(qs, many=True)
         return Response(serializer.data)
 
 
